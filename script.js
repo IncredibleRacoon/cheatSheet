96265f5cd70d7f13c6561ddfde2c32e0e490d2f7
IncredibleRacoon <78176838+IncredibleRacoon@users.noreply.github.com>
2022-06-17 20:22:29 +0200

    Add barebone dynamic chapter structure

--- script.js
+++ script.js
@@ -9,5 +9,18 @@ function startFunction() {
     document.getElementById("contentContainer").addEventListener("mouseover", () => {
         document.getElementById("chapterContainer").style.width = "0px";
     });
+
+    createChapterContent();
+
+}
+
+const createChapterContent = () => {
+    const chapterContent = document.getElementById("chapterContent");
+    const allChapters = document.querySelectorAll("h1, h2, h3");
+    for (chapter of allChapters) {
+        let headerCreated = document.createElement("h" + chapter.tagName.toString()[1]);
+        headerCreated.innerHTML = chapter.innerHTML;
+        chapterContent.appendChild(headerCreated);
+    }
 }
 

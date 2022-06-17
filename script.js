d1b1ea911180f937f153f48e17b9ec8cf11f247f
IncredibleRacoon <78176838+IncredibleRacoon@users.noreply.github.com>
2022-06-17 17:49:18 +0200

    Add chapter opening and closing

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+function startFunction() {
+    document.getElementById("chapterContainer").addEventListener("mouseover", () => {
+        document.getElementById("chapterContainer").style.width = "500px";
+    });
+    document.getElementById("menubarContainer").addEventListener("mouseover", () => {
+        document.getElementById("chapterContainer").style.width = "0px";
+    });
+    document.getElementById("contentContainer").addEventListener("mouseover", () => {
+        document.getElementById("chapterContainer").style.width = "0px";
+    });
+}
+

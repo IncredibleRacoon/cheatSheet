fa30930cc022658fa2d643de23e2eb9c4ab904bf
IncredibleRacoon <78176838+IncredibleRacoon@users.noreply.github.com>
2022-06-17 20:44:14 +0200

    Tweak chapter styling

--- script.js
+++ script.js
@@ -16,11 +16,11 @@ function startFunction() {
 
 const createChapterContent = () => {
     const chapterContent = document.getElementById("chapterContent");
-    const allChapters = document.querySelectorAll("h1, h2, h3");
+    const allChapters = document.querySelectorAll("h2, h3");
     for (chapter of allChapters) {
         let headerCreated = document.createElement("h" + chapter.tagName.toString()[1]);
         headerCreated.innerHTML = chapter.innerHTML;
+        headerCreated.style.marginLeft = 20 * (chapter.tagName.toString()[1] - 1) + "px";
         chapterContent.appendChild(headerCreated);
     }
 }
-
